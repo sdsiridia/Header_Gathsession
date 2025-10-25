0133e5a1acfe08c9a189d98125013b218f097c61
sdsiridia <pablorossa@gmail.com>
2025-10-25 09:38:43 +0200

    build v1

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base:"/Header_Gathsession/",
+    base: "./", //al agreagar el . usa rutas relativas  en los enlaces generados por vite
+});
